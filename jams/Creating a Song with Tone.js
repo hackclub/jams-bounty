3c0e532e7f18bd071431b148f9f0ee1b8704dcf4
Paul Trusov <33201430+nerap08@users.noreply.github.com>
2023-07-01 16:43:53 -0400

    Create Creating a Song with Tone.js

---
+++ jams/Creating a Song with Tone.js
@@ -0,0 +1,34 @@
+Summary: Members will utilize Tone.js and the Replit coding environment to compose their own unique songs. They will learn to create music by coding in javascript.
+
+Outcome: Members will have a deep understanding of music production through coding and will have created their own original melody using Tone.js.
+
+Requirements: Access to Replit.
+
+Language used: JavaScript (Tone.js library)
+
+Customization opportunity: Participants will have the opportunity to express themselves by composing their own unique songs. They can experiment with different instruments, rhythms, and melodies to create a song.
+
+Platform Limitations: No limitations as long as members have access to a replit.
+
+Time estimation: 1 hour or less
+
+Why should Hack Clubbers care about this project?
+This project combines the worlds of music and coding, and It teaches members how to use javascript.
+
+What makes this workshop fun or interesting for Club Members?
+Club members will have the chance to their own songs that they would get to share with one another. 
+  
+What platforms will be supported (i.e. MacOS, Windows, Chromebook, Mobile, Browser, etc)?
+Any device that has access to Replit.
+
+How will you allow Club Leaders to add their own project to the Jam presentation (giving them a sense of ownership over the meeting)?
+Club leaders could choose the inspiration for the songs created during the Jam.
+  
+General outline: 
+1. Introduction to Tone.js and its capabilities in music production.
+2. Setting up a Replit project with Tone.js library included.
+3. Understanding the basics of music synthesis, including instruments, oscillators, and filters.
+4. Adding beats and rhythms using Tone.js's drum machine or sampler.
+5. Manipulating and modulating sounds with different effects.
+6. Fine-tuning the song by adjusting tempo, volume, and other parameters.
+7. Sharing the created songs within the Jam session.
